2260dfaaa95cb6ea1aca87abad7e28bc5cdee1ef
Eric Mueller <emueller@emcien.com>
2013-04-30 17:55:50 -0400

    show only issues for this repo

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -7,6 +7,11 @@ class Repo
   end
 
   def issues
-    []
+    binding.pry
+    self.github.issues.list({
+      :user => $organization,
+      :repo => self.name,
+      :filter => 'all',
+      :per_page => 100})
   end
 end
